d6b1b3bc5fb89cc220fa1a8ff7682d49dd0addc9
timhams <timtam54@hotmail.com>
2025-12-28 09:59:15 +1000

    Add accounting.reports.read scope (without bankfeeds which requires partnership)

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: NextRequest) {
         companyId: COMPANY_ID,
         clientId: client_id,
         clientSecret: client_secret,
-        scope: scope || 'payroll.employees payroll.timesheets accounting.settings accounting.attachments accounting.transactions accounting.contacts payroll.settings offline_access',
+        scope: scope || 'payroll.employees payroll.timesheets accounting.settings accounting.attachments accounting.transactions accounting.contacts accounting.reports.read payroll.settings offline_access',
       }
     })
 
